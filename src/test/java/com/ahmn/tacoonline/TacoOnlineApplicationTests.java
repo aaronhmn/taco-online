427e81f7c44d52b110bd965fa3ea8163a211420f
Aarón Helices Martín-Niño <aaronhelices@gmail.com>
2025-06-23 14:05:30 +0200

    Creación del proyecto

---
+++ src/test/java/com/ahmn/tacoonline/TacoOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmn.tacoonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
