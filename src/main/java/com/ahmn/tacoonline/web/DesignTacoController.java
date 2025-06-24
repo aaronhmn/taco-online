67538a799c013710c0ffef2a897352fd2ce7453d
Aarón Helices Martín-Niño <aaronhelices@gmail.com>
2025-06-24 11:24:49 +0200

    logica y error en el form

--- src/main/java/com/ahmn/tacoonline/web/DesignTacoController.java
+++ src/main/java/com/ahmn/tacoonline/web/DesignTacoController.java
@@ -3,8 +3,14 @@ package com.ahmn.tacoonline.web;
 import com.ahmn.tacoonline.model.Ingredient;
 import com.ahmn.tacoonline.model.Ingredient.Type;
 import com.ahmn.tacoonline.model.Taco;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -12,11 +18,42 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Controller
 @RequestMapping("/design")
 public class DesignTacoController {
 
+    @GetMapping
     public String showDesignForm(Model model) {
+        fillModelWithIngredients(model);
+        //model.addAttribute("tktn", new Taco() );
+
+        return "design";
+    }
+
+    @ModelAttribute(name = "tktn")
+    public Taco taco() {
+        return new Taco();
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(ing -> ing.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public String processDesign(@Valid @ModelAttribute(name = "tktn") Taco design, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            fillModelWithIngredients(model);
+            return "design";
+        }
+        log.info("Processing Design Taco: {}", design);
+        return "redirect:/orders/current";
+    }
+
+    public void fillModelWithIngredients(Model model) {
         List<Ingredient> ingredients = Arrays.asList(
                 new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
                 new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
@@ -35,16 +72,5 @@ public class DesignTacoController {
         for(Type type : types) {
             model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
         }
-
-        model.addAttribute("tktn", new Taco() );
-
-        return "design";
-    }
-
-    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
-        return ingredients
-                .stream()
-                .filter(ing -> ing.getType().equals(type))
-                .collect(Collectors.toList());
     }
 }
