b4367f83cac36940fc9311124d0870632445906e
Aarón Helices Martín-Niño <aaronhelices@gmail.com>
2025-06-24 12:52:00 +0200

    Añadido errores para el formulario de order

--- src/main/java/com/ahmn/tacoonline/model/Order.java
+++ src/main/java/com/ahmn/tacoonline/model/Order.java
@@ -3,16 +3,22 @@ package com.ahmn.tacoonline.model;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+@Data
 public class Order {
 
-    @NotBlank(message = "El nombre es oblibatorio")
-    private String name;
-    private String street;
-    private String city;
-    private String state;
-    private String zip;
+    @NotBlank(message = "El nombre es obligatorio")
+    private String deliveryName;
+    @NotBlank(message = "La calle es obligatoria")
+    private String deliveryStreet;
+    @NotBlank(message = "La ciudad es obligatoria")
+    private String deliveryCity;
+    @NotBlank(message = "El state es obligatorio")
+    private String deliveryState;
+    @NotBlank(message = "El zip es obligatorio")
+    private String deliveryZip;
 
     @CreditCardNumber(message = "¡No es una tarjeta válida!")
     private String ccNumber;
