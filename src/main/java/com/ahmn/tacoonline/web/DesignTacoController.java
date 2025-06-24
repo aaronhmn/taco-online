24d1d2310d9186367bcd5041a046a7307507eb66
Aarón Helices Martín-Niño <aaronhelices@gmail.com>
2025-06-24 10:01:05 +0200

    Agregamos la lógica del controller del diseño

--- src/main/java/com/ahmn/tacoonline/web/DesignTacoController.java
+++ src/main/java/com/ahmn/tacoonline/web/DesignTacoController.java
@@ -1,10 +1,50 @@
 package com.ahmn.tacoonline.web;
 
+import com.ahmn.tacoonline.model.Ingredient;
+import com.ahmn.tacoonline.model.Ingredient.Type;
+import com.ahmn.tacoonline.model.Taco;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/design")
 public class DesignTacoController {
 
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+        );
+
+        Type[] types = Type.values();
+
+        for(Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+        }
+
+        model.addAttribute("tktn", new Taco() );
+
+        return "design";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(ing -> ing.getType().equals(type))
+                .collect(Collectors.toList());
+    }
 }
