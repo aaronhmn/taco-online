442c10891527fcbede2228216b05de86c3ce10db
Aarón Helices Martín-Niño <aaronhelices@gmail.com>
2025-06-25 14:14:02 +0200

    Control de paginas error

---
+++ src/main/java/com/ahmn/tacoonline/web/MyErrorController.java
@@ -0,0 +1,34 @@
+package com.ahmn.tacoonline.web;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class MyErrorController implements ErrorController {
+
+    final ErrorAttributes errorAttributes;
+    private final WebRequest webRequest;
+
+    @RequestMapping("/error")
+    public String handleError(WebRequest request, Model model) {
+        log.error("Fallo de la app");
+        Map<String, Object> errorAttributesMap = errorAttributes
+                .getErrorAttributes(webRequest, ErrorAttributeOptions.of(ErrorAttributeOptions.Include.STACK_TRACE, ErrorAttributeOptions.Include.MESSAGE, ErrorAttributeOptions.Include.PATH));
+
+        model.addAttribute("msg", errorAttributesMap.get("error"));
+        model.addAttribute("url", errorAttributesMap.get("path"));
+
+        return "error";
+    }
+}
